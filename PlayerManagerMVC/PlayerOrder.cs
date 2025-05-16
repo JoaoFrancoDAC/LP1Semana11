816124e7d3509dfe33aff934b7c044341beea370
JoaoFrancoDAC <jvfdc15@gmail.com>
2025-05-16 14:32:44 +0100

    MVC implemented

---
+++ PlayerManagerMVC/PlayerOrder.cs
@@ -0,0 +1,9 @@
+namespace PlayerManagerMVC
+{
+    public enum PlayerOrder
+    {
+        ByScore = 0,
+        ByName = 1,
+        ByNameReverse = 2
+    }
+}
---
+++ PlayerManagerMVC/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace PlayerManagerMVC
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Controller controller = new Controller();
+            PlayerView view = new PlayerView();
+
+            bool exit = false;
+            while (!exit)
+            {
+                view.ShowMenu();
+                string option = Console.ReadLine();
+
+                switch (option)
+                {
+                    case "1":
+                        string name = view.GetPlayerName();
+                        int score = view.GetPlayerScore();
+                        controller.AddPlayer(name, score);
+                        break;
+                    case "2":
+                        view.ListPlayers(controller.GetAllPlayers());
+                        break;
+                    case "3":
+                        int minScore = view.GetMinimumScore();
+                        view.ListPlayers(controller.GetPlayersWithScoreGreaterThan(minScore));
+                        break;
+                    case "4":
+                        PlayerOrder order = view.GetSortOrder();
+                        controller.SortPlayers(order);
+                        break;
+                    case "0":
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option!");
+                        break;
+                }
+
+                if (!exit)
+                {
+                    Console.WriteLine("\nPress any key to continue...");
+                    Console.ReadKey(true);
+                    Console.Clear();
+                }
+            }
+        }
+    }
+}
